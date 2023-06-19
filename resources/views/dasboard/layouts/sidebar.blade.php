a1d5f7e663bae532d7a50584854407cfcf3d78f4
Riska Ardila <riskaardila35@gmail.com>
2023-06-19 17:06:15 +0700

    update

--- resources/views/dasboard/layouts/sidebar.blade.php
+++ resources/views/dasboard/layouts/sidebar.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="tables.css">
+{{-- <link rel="stylesheet" href="tables.css">
 <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
     <div class="offcanvas-lg offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu"
         aria-labelledby="sidebarMenuLabel">
@@ -44,4 +44,38 @@
             </ul>
         </div>
     </div>
+</div> --}}
+
+<div class="sidebar">
+    <center>
+        <img src="img/profile.png" class="profile_image">
+        <h4>Admin</h4>
+    </center>
+    <a class="nav-link {{ Request::is('dasboard') ? 'active' : '' }}" aria-current="page" href="/dasboard">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  class="bi bi-house-door"
+            viewBox="0 0 16 16">
+            <path
+                d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z" />
+        </svg>
+        Dashboard
+    </a>
+    <a class="nav-link {{ Request::is('uang-masuk*') ? 'active' : '' }}" aria-current="page" href="/uang-masuk">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-plus"
+            viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+                d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+            <path
+                d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+        </svg>
+        Uang Masuk
+    </a>
+    <a class="nav-link {{ Request::is('uang-keluar*') ? 'active' : '' }}" aria-current="page" href="/uang-keluar">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-dash"
+            viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M5.5 10a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z" />
+            <path
+                d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+        </svg>
+        Uang Keluar
+    </a>
 </div>
